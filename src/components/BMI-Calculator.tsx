92a9d827e7c5ffab0436e2e19741b90c83f26520
weemizo <mrceleius@gmail.com>
2023-10-24 18:23:49 +0200

    Added BMI Calculator

--- src/components/BMI-Calculator.tsx
+++ src/components/BMI-Calculator.tsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 
 export default function BMI() {
-    const [weight, setWeight] = useState<number>(0)
+    const [weight, setWeight] = useState<number>(0);
+
+    const [height, setHeight] = useState<number>(0);
     
     const weightAssign = (weight: number) => {
-        return Number.isNaN(weight) ? "Type in your weight" : `Your weight is ${weight}kg`
+        return Number.isNaN(weight) ? "Type in your weight" : `Your weight is ${weight}kgs`
+    }
+
+    const heightAssign = (height: number) => {
+        return Number.isNaN(height) ? "Type in your height" : `Your height is ${height}m`
+    }
+
+    const bmiCalc = (height: number, weight: number) => {
+        const bmi = (weight/Math.pow(height, 2)).toFixed(2);
+        return Number.isNaN(bmi) ? "Please type in correct numbers" : `Your BMI is ${bmi}`
     }
 
     return (
         <div>
+            <div className='text-xl font-bold mb-10'> This app currently supports only the metric system. [kilograms / meters] </div>
+            <div>{weightAssign(weight)}</div>
+
             <form>
                 <input
                     type="number"
@@ -17,7 +31,18 @@ export default function BMI() {
                     onChange={(e) => setWeight(parseInt(e.target.value))}
                 />
             </form>
-            <div>{weightAssign(weight)}</div>
+
+            <div>{heightAssign(height)}</div>
+            <form>
+                <input
+                    type="number"
+                    name="height"
+                    value={height}
+                    onChange={(e) => setHeight(parseFloat(e.target.value))}
+                />
+            </form>
+
+            <div className='text-3xl font-bold m-10'>{bmiCalc(height, weight)}</div>
         </div>
     );
 }
