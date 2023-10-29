c26f93c51bee9c1a8be0230df6b963ca306e6633
weemizo <mrceleius@gmail.com>
2023-10-29 11:19:12 +0100

    fix(BMI-Calculator): fix the bug of the BMI calculator

--- src/components/BMI-Calculator/BMI-Calculator.tsx
+++ src/components/BMI-Calculator/BMI-Calculator.tsx
@@ -24,7 +24,6 @@ export default function BMI() {
         <div>
             <DarkModeIcon/>
             <div className='text-xl font-bold mb-10'> This app currently supports only the metric system. [kilograms / meters] </div>
-            <div>{Number.isNaN(weight) || height<=0 ? "Type in your weight" : `Your weight is ${weight}kgs`}</div>
             <form onSubmit={handleSubmit}>
                 <input
                     step="any"
@@ -32,7 +31,7 @@ export default function BMI() {
                     name="weight"
                     min="0.01"
                     max="600"
-                    value={weight}
+                    placeholder="Type in your weight"
                     onChange={e => setWeight(parseFloat(e.target.value))}
                 />
                 <input
@@ -41,13 +40,13 @@ export default function BMI() {
                     name="height"
                     min="0.01"
                     max="3"
-                    value={height}
-                    onChange={(e) => setHeight(parseFloat(e.target.value))}
+                    placeholder="Type in your height"
+                    onChange={e => setHeight(parseFloat(e.target.value))}
                 />
                 {/* text-black dark:text-white bg-cyan-100 dark:bg-zinc-900 */}
                 <button type="submit" className=' dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:border-zinc-800 dark:hover:border-zinc-700 text-black dark:text-white bg-cyan-200 hover:bg-cyan-300 font-bold py-2 px-4 border-b-4 border-cyan-400 hover:border-cyan-600 rounded'> Submit </button>
             </form>
-            <div>{Number.isNaN(height)|| height<=0 ? "Type in your height" : `Your height is ${height}m`}</div>
+            <div>{Number.isNaN(height)|| Number.isNaN(weight) || weight<=0 || height<=0 ? "Type in your measurement" : `You are ${height}m tall & weigh ${weight}kgs`}</div>
             <div className='text-3xl font-bold m-10'>{message}</div>
         </div>
     );
