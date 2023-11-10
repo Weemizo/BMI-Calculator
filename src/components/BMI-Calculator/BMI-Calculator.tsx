2dbe31af13ac6b33eb0351264a9c1ec15985d064
weemizo <mrceleius@gmail.com>
2023-11-10 18:58:44 +0100

    feature(BMICalc): Added messages

--- src/components/BMI-Calculator/BMI-Calculator.tsx
+++ src/components/BMI-Calculator/BMI-Calculator.tsx
@@ -23,14 +23,28 @@ export default function BMI() {
 
     const bmi = system === "metric" ? metric : imperial;
 
+    const bmiMessage = `Your BMI is ${bmi}`;
+    
+    const bmiResult = () => {
+      if (parseInt(bmi)>=30) {
+        return `You're morbidly obese, contact your doctor immediately. ${bmiMessage}`
+      } else if (parseInt(bmi)>=25 || parseInt(bmi)<30) {
+        return `You're obese. ${bmiMessage}`
+      } else if (parseInt(bmi)>=18.5 || parseInt(bmi)<25) {
+        return `Normal. ${bmiMessage}`
+      } else if (parseInt(bmi)>=17.5 || parseInt(bmi)<18.5) {
+        return `You're underweight, a little bit. ${bmiMessage}`
+      } else `You're severly underweight. ${bmiMessage}`
+    }
+
     return Number.isNaN(bmi)
       ? "Please type in correct numbers"
-      : `Your BMI is ${bmi}`;
+      : bmiResult();
   };
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    setMessage(bmiCalc);
+    setMessage(bmiCalc() || "Please type in correct numbers");
   };
 
   const wPlaceholder = system === "metric" ? "kg" : "lbs";
@@ -71,7 +85,7 @@ export default function BMI() {
           Submit
         </button>
       </form>
-      <div className="font-bold m-5">
+      <div className="font-bold m-5 text-center">
         {Number.isNaN(height) ||
         Number.isNaN(weight) ||
         weight <= 0 ||
@@ -79,15 +93,21 @@ export default function BMI() {
           ? "Type in your measurement"
           : `You are ${height} ${hPlaceholder} tall & weigh ${weight} ${wPlaceholder}`}
       </div>
-      <div className="flex flex-col justify-center text-3xl font-bold m-10 items-center">
+      <div className="flex flex-col justify-center text-3xl font-bold m-10 items-center mx-auto">
         {message}
-        <input
-          type="checkbox"
-          onChange={() =>
-            setSystem(system === "metric" ? "imperial" : "metric")
-          }
-        />
-        Check, if Imperial
+        <label className="mt-10 relative inline-flex items-center mb-5 cursor-pointer">
+          <input
+            type="checkbox"
+            onChange={() =>
+              setSystem(system === "metric" ? "imperial" : "metric")
+            }
+            className="sr-only peer"
+          />
+          <div className=" w-9 h-5 bg-cyan-300 rounded-full peer dark:bg-zinc-500 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-zinc-400 peer-checked:bg-black dark:peer-checked:bg-zinc-700"></div>
+          <span className="ms-3 text-sm font-bold text-gray-900 dark:text-gray-300">
+            Imperial
+          </span>
+        </label>
       </div>
     </div>
   );
